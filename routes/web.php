5eb05ac529290a2e5af0cbef6e074a2dcda5c3cd
Zeus-M <ahmadmohib127@gmail.com>
2024-07-03 11:55:54 +0500

    Chat Commit

--- routes/web.php
+++ routes/web.php
@@ -46,18 +46,3 @@ Route::put('/messages/{id}', [MessagesController::class, 'update'])->name('messa
 Route::delete('/messages/{id}', [MessagesController::class, 'destroy'])->name('messages.destroy');
 
 Route::post('/save-react', [MessagesController::class, 'saveReact'])->name('message-comments.saveReact');
-
-
-
-
- 
-
-
-
-
- 
-
-
-
- 
-
